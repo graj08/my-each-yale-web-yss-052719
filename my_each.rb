4084888a1f33ad12fe090c47335245fe02f3c8e7
gorgeous-karma-7335 <graj08@gmail.com>
2019-05-14 01:08:35 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,18 @@
-def my_each # put argument(s) here
+def my_each (array)
+  # put argument(s) here
   # code here
+  i=0
+  return_array = Array.new
+  while i<array.length
+    yield array[i]
+    return_array << array[i]
+    i+=1
+  end
+  return_array
+end
+
+collection = [1, 2, 3, 4]
+return_array = Array.new
+my_each(collection) do |i|
+  return_array << collection[i]
 end
