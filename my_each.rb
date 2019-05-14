9b0fb9c89795d60cb2f464010be5048b8e463150
gorgeous-karma-7335 <graj08@gmail.com>
2019-05-14 01:08:19 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -2,10 +2,13 @@ def my_each (array)
   # put argument(s) here
   # code here
   i=0
+  return_array = Array.new
   while i<array.length
     yield array[i]
+    return_array << array[i]
     i+=1
   end
+  return_array
 end
 
 collection = [1, 2, 3, 4]
